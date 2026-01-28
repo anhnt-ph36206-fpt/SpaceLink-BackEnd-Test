cb74069c70a2456ed272f9a38fecc18b779e24b9
anhnt-ph36206-fpt <anhntph36206@fpt.edu.vn>
2026-01-29 00:59:34 +0700

    Add Sanctum for API and test Login with Token

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    if (!Auth::attempt($credentials)) {
+        return response()->json([
+            'message' => 'Invalid credentials'
+        ], 401);
+    }
+
+    $user = $request->user();
+
+    // (optional) xoá token cũ
+    $user->tokens()->delete();
+
+    $token = $user->createToken('api-token')->plainTextToken;
+
+    return response()->json([
+        'token' => $token,
+        'user' => $user,
+    ]);
+});
+
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return $request->user();
+});
