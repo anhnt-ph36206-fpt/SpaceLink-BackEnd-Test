fdc1bfa26bb1aabe88e21aa0db4e47cf8a1d0813
anhnt-ph36206-fpt <anhntph36206@fpt.edu.vn>
2026-01-29 10:40:26 +0700

    Install Cors and config basic

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Global Prepend Middleware
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
